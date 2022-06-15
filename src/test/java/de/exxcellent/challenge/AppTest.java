e349a3bc8ae3e0910b40fe910854490b0a93fbfc
Momo <hohenheim@mailbox.org>
2022-06-15 18:26:11 +0200

    Test App use arguments

--- src/test/java/de/exxcellent/challenge/AppTest.java
+++ src/test/java/de/exxcellent/challenge/AppTest.java
@@ -1,31 +1,50 @@
 package de.exxcellent.challenge;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 /**
  * Example JUnit 5 test case.
+ * 
  * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
  */
 class AppTest {
 
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
     private String successLabel = "not successful";
 
     @BeforeEach
-    void setUp() {
-        successLabel = "successful";
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
     }
 
     @Test
-    void aPointlessTest() {
-        assertEquals("successful", successLabel, "My expectations were not met");
+    void runWeather() {
+        App.main("--weather", "src/main/resources/de/exxcellent/challenge/weather.csv");
+        String expect = "Day with smallest temperature spread : 14";
+        assertEquals(expect, outputStreamCaptor.toString().trim());
     }
 
     @Test
     void runFootball() {
-        App.main("--football", "football.csv");
+        App.main("--football", "src/main/resources/de/exxcellent/challenge/football.csv");
+        String expect = "Team with smallest goal spread : Aston_Villa";
+        assertEquals(expect, outputStreamCaptor.toString().trim());
     }
 
 }
