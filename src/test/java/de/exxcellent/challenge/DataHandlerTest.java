ee977575ba8b07dd6f007a7476920182a43e97c9
Momo <hohenheim@mailbox.org>
2022-06-15 13:22:33 +0200

    Test DataHandler accepts Strings instead of floats with headers

--- src/test/java/de/exxcellent/challenge/DataHandlerTest.java
+++ src/test/java/de/exxcellent/challenge/DataHandlerTest.java
@@ -32,40 +32,51 @@ class DataHandlerTest {
 
     @Test
     void dataHandler_getSmallestSpread_1_onlyPositivValues() {
-        List<Float[]> floatList = new ArrayList<Float[]>();
-        Float[] v1 = { 1f, 50f, 49f };
-        Float[] v2 = { 2f, 10f, 0f };
-        Float[] v3 = { 3f, 299999999f, 2f };
-        floatList.add(v1);
-        floatList.add(v2);
-        floatList.add(v3);
+        List<String[]> stringList = new ArrayList<String[]>();
+        String[] head = { "Day", "MxT", "MnT" };
+        String[] v1 = { "1", "50", "49" };
+        String[] v2 = { "2", "10", "0" };
+        String[] v3 = { "3", "299999999", "2" };
+        stringList.add(head);
+        stringList.add(v1);
+        stringList.add(v2);
+        stringList.add(v3);
 
-        var dataHandler = new DataHandler(floatList);
-        var day = dataHandler.getSmallestSpread();
+        var dataHandler = new DataHandler(stringList);
+        var day = dataHandler.getSmallestSpread("Day", "MxT", "MnT");
 
-        assertEquals(1, day);
+        assertEquals("1", day);
     }
 
     @Test
     void dataHandler_getSmallestSpread_2_withNegativValues() {
-        List<Float[]> floatList = new ArrayList<Float[]>();
-        Float[] v1 = { 1f, 50f, -1f };
-        Float[] v2 = { 2f, -4f, -5f };
-        Float[] v3 = { 3f, 10f, 2f };
-        floatList.add(v1);
-        floatList.add(v2);
-        floatList.add(v3);
+        List<String[]> stringList = new ArrayList<String[]>();
+        String[] head = { "Day", "MxT", "MnT" };
+        String[] v1 = { "1", "50", "-1" };
+        String[] v2 = { "2", "-4", "-5" };
+        String[] v3 = { "3", "10", "2" };
+        stringList.add(head);
+        stringList.add(v1);
+        stringList.add(v2);
+        stringList.add(v3);
 
-        var dataHandler = new DataHandler(floatList);
-        var day = dataHandler.getSmallestSpread();
+        var dataHandler = new DataHandler(stringList);
+        var day = dataHandler.getSmallestSpread("Day", "MxT", "MnT");
 
-        assertEquals(2, day);
+        assertEquals("2", day);
     }
 
     @Test
     void dataHandler_getSmallestSpread_ByFileWether() {
         var dataHandler = new DataHandler("src/main/resources/de/exxcellent/challenge/weather.csv");
-        var day = dataHandler.getSmallestSpread();
-        assertEquals(14, day);
+        var day = dataHandler.getSmallestSpread("Day", "MxT", "MnT");
+        assertEquals("14", day);
+    }
+
+    @Test
+    void dataHandler_getSmallestSpread_ByFootball() {
+        var dataHandler = new DataHandler("src/main/resources/de/exxcellent/challenge/football.csv");
+        var team = dataHandler.getSmallestSpread("Team", "Goals", "Goals Allowed");
+        assertEquals("Aston_Villa", team);
     }
 }
