3d9e0d313d9c5da224231dc91cd65a032f85d8f5
Momo <hohenheim@mailbox.org>
2022-06-15 13:28:35 +0200

    DataHandler works with StringArray as Datatyp

--- src/main/java/de/exxcellent/challenge/DataHandler.java
+++ src/main/java/de/exxcellent/challenge/DataHandler.java
@@ -6,24 +6,24 @@ public class DataHandler {
     private int DAY_Index = 0;
     private int MAX_Temp_Index = 1;
     private int MIN_Temp_Index = 2;
-    private List<Float[]> data;
+    private List<String[]> data;
 
-    public DataHandler(List<Float[]> data) {
+    public DataHandler(List<String[]> data) {
         this.data = data;
     }
 
     public DataHandler(String filename) {
-        this.data = CsvReader.readCsvFileToListFloatArray(filename);
+        this.data = CsvReader.readCsvFileToListStringArrays(filename);
     }
 
-    public int getSmallestSpread() {
-        int indexSmallestSpread = -1;
+    public String getSmallestSpread() {
+        String indexSmallestSpread = null;
         float smallestSpread = Float.MAX_VALUE;
-        for (Float[] day : this.data) {
-            var delta = day[MAX_Temp_Index] - day[MIN_Temp_Index];
-            System.out.println(Math.round(day[DAY_Index]) + " " + delta);
+        for (String[] day : this.data) {
+            var delta = Float.parseFloat(day[MAX_Temp_Index]) - Float.parseFloat(day[MIN_Temp_Index]);
+            System.out.println(day[DAY_Index] + " " + delta);
             if (delta < smallestSpread) {
-                indexSmallestSpread = Math.round(day[DAY_Index]);
+                indexSmallestSpread = day[DAY_Index];
                 smallestSpread = delta;
             }
         }
