51cace60b57e46c5fda21575706854d13ab9b0b0
Momo <hohenheim@mailbox.org>
2022-06-16 21:50:10 +0200

    DataHandler comments

--- src/main/java/de/exxcellent/challenge/DataHandler.java
+++ src/main/java/de/exxcellent/challenge/DataHandler.java
@@ -4,16 +4,34 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Can handle a table as List<String[]>.
+ * Can calculate the smallest spread between values in two columns in this
+ * table.
+ */
 public class DataHandler {
     private List<String[]> data;
     private String[] headers;
 
+    /**
+     * Constructor
+     * Initialize DataHandler by a data in a List of String-Arrays.
+     * 
+     * @param data
+     */
     public DataHandler(List<String[]> data) {
         this.headers = data.get(0);
         data.remove(0);
         this.data = data;
     }
 
+    /**
+     * Constructor
+     * Initialize DataHandler by a file and it's path.
+     * 
+     * @param filename relativ path to file
+     * @throws Exception FileNotFoundException if the path is not valid
+     */
     public DataHandler(String filename) throws Exception {
         var data = CsvReader.readCsvFileToListStringArrays(filename);
         if (data == null)
@@ -23,6 +41,15 @@ public class DataHandler {
         this.data = data;
     }
 
+    /**
+     * Calculate the smallest spread of the data between firstValue column and
+     * secValue column and returns the value of the index column
+     * 
+     * @param index      header name of index column
+     * @param firstValue header name of the first value column
+     * @param secValue   header name of the second value column
+     * @return value of the index column with the smallest spread
+     */
     public String getSmallestSpread(String index, String firstValue, String secValue) {
         String indexSmallestSpread = null;
         float smallestSpread = Float.MAX_VALUE;
@@ -41,6 +68,12 @@ public class DataHandler {
         return indexSmallestSpread;
     }
 
+    /**
+     * Get the index of the header
+     * 
+     * @param header name of the header
+     * @return index of the column with this header name
+     */
     private int columnFromHeader(String header) {
         return Arrays.asList(headers).indexOf(header);
     }
